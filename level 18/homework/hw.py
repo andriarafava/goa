2ff2c671f23cc92e582db11c8b59e2ab7bc3b4e3
andriarafava <andriarafava03@gmail.com>
2024-12-10 09:07:04 -0800

    second commit

--- level 18/homework/hw.py
+++ level 18/homework/hw.py
@@ -38,3 +38,48 @@ while True:
 
 # Display the count of incorrect attempts
 print(f"Total incorrect attempts: {incorrect_attempts}")
+
+
+
+number = float(input("Enter a number: "))
+
+# Check the number's status
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+
+    # მომხმარებლის მიერ რიცხვის შეყვანა
+number = int(input("შეიყვანეთ რიცხვი: "))
+
+# შემოწმება თუ რიცხვი 5-ზე უნაშთოდ იყოფა
+if number % 5 == 0:
+    print("რიცხვი 5-ზე უნაშთოდ იყოფა.")
+else:
+    print("რიცხვი 5-ზე უნაშთოდ არ იყოფა.")
+
+    # Correct password
+correct_password = "Goa best"
+
+# Counter for incorrect attempts
+incorrect_attempts = 0
+
+# Loop to prompt user until the correct password is entered
+while True:
+    # Ask the user to enter the password
+    password = input("Enter the password: ")
+    
+    # Check if the entered password is correct
+    if password == correct_password:
+        print("Correct password! Access granted.")
+        break
+    else:
+        # Increment incorrect attempts and notify the user
+        incorrect_attempts += 1
+        print("Incorrect password. Try again.")
+
+# Print the total number of incorrect attempts
+print(f"Number of incorrect attempts: {incorrect_attempts}")
