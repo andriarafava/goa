a589815ecf9e7375984c7e3622c9bd718fa0f5d4
andriarafava <andriarafava03@gmail.com>
2024-12-24 11:06:51 -0800

    second commit

--- level 22/homework/hw.py
+++ level 22/homework/hw.py
@@ -33,4 +33,22 @@ words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
 every_second_word = words[::2]
 
 
-print("Every second word:", every_second_word)
+print( every_second_word)
+
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+num1 = int(input((num1) ))
+num2 = int(input((num2)))
+
+if num1 > num2:
+    result = data[num1:num2]
+elif num2 > num1:
+    result = data[num2:num1]
+else:
+
+    result = []
+
+
